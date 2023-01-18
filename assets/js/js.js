5245800bc3e02754a3d7092b781a1115b38841ec
Lucas Heiler <110560832+LucasHeiler24@users.noreply.github.com>
2023-01-18 15:51:16 -0300

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -46,3 +46,22 @@ $('#btn-pyton').click(
         $('#caixa-6').toggle()
     }
 )
+const btnMobile = document.getElementById('menu-mobile');
+
+function toggleMenu(){
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+function menuAbre(){
+    let ul = document.querySelector('#navbar ul');
+
+    if(ul.classList.contains("open")){
+        ul.classList.remove("open");
+    }
+    else {
+        ul.classList.add("open");
+    }
+}
